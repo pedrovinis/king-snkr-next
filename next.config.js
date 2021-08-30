272ede8ef89a03c661ca73b74ccd73365a2656ce
Pedro Vinícius <62823394+pedrovinis@users.noreply.github.com>
2021-08-30 14:08:44 -0300

    Bugs Fixed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,40 @@
+const { config } = require("process")
+
 module.exports = {
   images: {
     domains: [
+      '/',
       'pxv.vercel.app',
       'pxv.app',
       'images.lojanike.com.br',
       'localhost'
     ],
     imageSizes: [24, 64, 300]
+  },
+  webpack: (webpackConfig, { isServer }) => {
+    const { module } = webpackConfig;
+    return {
+      ...webpackConfig,
+      module: {
+        ...module,
+        rules: [
+          ...module.rules,
+          {
+            test: /\.(png|gif|jpg|jpeg)$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  emitFile: isServer,
+                  publicPath: `/_next/static/`,
+                  outputPath: `${isServer ? '../' : ''}static/`,
+                  name: '[path][name].[ext]'
+                }
+              }
+            ]
+          }
+        ]
+      }
+    }
   }
-};
+}
