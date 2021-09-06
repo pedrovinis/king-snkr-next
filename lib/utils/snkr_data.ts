4f8c8d1fb18b2f865a7473a7e94a7254059e0309
Pedro Vinícius <62823394+pedrovinis@users.noreply.github.com>
2021-09-06 17:22:26 -0300

    Bug Snkr Data fixed

--- lib/utils/snkr_data.ts
+++ lib/utils/snkr_data.ts
@@ -6,7 +6,7 @@ interface iDataLayer {
 
 
 export const getAndFormatSnkrData = async(link:string) => {
-    const res = await fetch("https://www.nike.com.br/ajko-1-153-169-211-348322", {
+    const res = await fetch(link, {
         "headers": {
           "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
           "accept-language": "en-US,en;q=0.9",
