cde099c4151ad65fa214e65583e4e12086f47c88
Pedro Vinícius <62823394+pedrovinis@users.noreply.github.com>
2021-09-10 21:57:00 -0300

    NProgress removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,14 @@
 import { SSRProvider, OverlayProvider } from 'react-aria'
-import Router from 'next/router'
 import '@styles/global.css'
-import '@styles/nprogress.css'
 import '@styles/chrome-bug.css'
 import '@styles/toastify.css'
 import type { AppProps } from 'next/app'
 import ResizeHandler from '@components/resize-handler'
 import { useEffect } from 'react'
 import { Slide, ToastContainer } from 'react-toastify'
-import NProgress from 'nprogress'
 import { AuthProvider } from '@components/auth-context'
 import { UserProductsProvider } from '@components/user-products-context'
 
-
-Router.events.on('routeChangeStart', ()=> {
-  NProgress.start()
-})
-
-Router.events.on('routeChangeComplete', () => NProgress.done())
-Router.events.on('routeChangeError', () => NProgress.done())
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
