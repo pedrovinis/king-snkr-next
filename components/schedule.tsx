898869e34b1c4e1224b4ba3c4a82f164512e7097
Pedro Vinícius <62823394+pedrovinis@users.noreply.github.com>
2021-09-15 17:52:47 -0300

    Schedule Update

--- components/schedule.tsx
+++ components/schedule.tsx
@@ -4,25 +4,15 @@ import styles from './schedule.module.css'
 import ScheduleCard from './schedule-card'
 
 function StageRow({ stage }: { stage: Stage }) {
-  // Group talks by the time block
-  const timeBlocks = stage.schedule.reduce((allBlocks: any, snkr) => {
-    allBlocks[snkr.release] = [...(allBlocks[snkr.release] || []), snkr];
-    return allBlocks
-  }, {})
-
   return (
     <div key={stage.name} className={styles.row}>
       <h3 className={cn(styles['stage-name'], styles[stage.slug])}>
         <span>{stage.name}</span>
       </h3>
       <div className={cn(styles.talks, styles[stage.slug])}>
-        {Object.keys(timeBlocks).map((startTime: string) => (
-          <div key={startTime}>
-            {timeBlocks[startTime].map((snkr: Snkr, index: number) => (
-              <ScheduleCard key={snkr.id} snkr={snkr} showTime={true} />
-            ))}
-          </div>
-        ))}
+        {stage.schedule.map( snkr => {
+          return <ScheduleCard key={snkr.id} snkr={snkr} showTime={true}/>
+        })}
       </div>
     </div>
   );
